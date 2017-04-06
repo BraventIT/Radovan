e6406f487e051b832befbc0a4d87050014403081
Mario López Baratas <mlopez@bravent.net>
2017-04-07 00:19:54 +0200

    Test project added

---
+++ Samples/Radovan.Sample/Radovan.SamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Radovan.Sample
+{
+	public partial class Radovan_SamplePage : ContentPage
+	{
+		public Radovan_SamplePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
