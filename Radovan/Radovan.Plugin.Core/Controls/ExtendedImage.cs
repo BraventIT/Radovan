e279eeb137b9f73a84973beeea06ae2432a13c23
ALBERTO FRAJ SOUTO <afraj@bravent.net>
2017-04-18 11:42:21 +0200

    Control Image
    Related Work Items: #14474

---
+++ Radovan/Radovan.Plugin.Core/Controls/ExtendedImage.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace Radovan.Plugin.Core.Controls
+{
+    public class ExtendedImage: Image
+    {
+        public static readonly BindableProperty SourcePlatformProperty = BindableProperty.Create(propertyName: "SourcePlatform",
+               returnType: typeof(ImageSource),
+               declaringType: typeof(ExtendedImage),
+               defaultValue: default(ImageSource),
+               propertyChanged: OnSourcePlatformChanged);
+
+        private static void OnSourcePlatformChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var SourcePlatform = newValue as FileImageSource;
+            string path = string.Empty;
+            switch (Device.OS)
+            {
+                case TargetPlatform.WinPhone:
+                case TargetPlatform.Windows:
+                    path = $"Assets/{SourcePlatform.File}.png"; break;
+                default:
+                    path = SourcePlatform.File; break;
+            }
+            SourcePlatform.File = path;
+            bindable.SetValue(SourceProperty, SourcePlatform);
+        }
+
+        public ImageSource SourcePlatform
+        {
+            get { return (ImageSource)GetValue(SourcePlatformProperty); }
+            set { SetValue(SourcePlatformProperty, value); }
+        }
+    }
+}
