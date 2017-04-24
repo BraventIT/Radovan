764298ab41b6ed3f101a4b41e35cf729548b51bc
Mario López Baratas <mlopez@bravent.net>
2017-04-25 00:18:49 +0200

    Tarea 14501:Control Button

---
+++ Radovan/Radovan.Plugin.Core/Controls/ImageButton.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq.Expressions;
+using Radovan.Plugin.Core.Enums;
+using Xamarin.Forms;
+
+namespace Radovan.Plugin.Core.Controls
+{
+	public class ImageButton : Button
+	{
+		public static readonly BindableProperty SourceProperty = BindableProperty.Create(
+			(Expression<Func<ImageButton, ImageSource>>)(w => w.Source),
+			null,
+			BindingMode.OneWay,
+			null,
+			(bindable, oldvalue, newvalue) => ((VisualElement)bindable).ToString());
+
+		public static readonly BindableProperty DisabledSourceProperty = BindableProperty.Create(
+			(Expression<Func<ImageButton, ImageSource>>)(w => w.DisabledSource),
+			null,
+			BindingMode.OneWay,
+			null,
+			(bindable, oldvalue, newvalue) => ((VisualElement)bindable).ToString());
+
+		public static readonly BindableProperty ImageWidthRequestProperty =
+			BindableProperty.Create(
+					nameof(ImageWidthRequest), typeof(int), typeof(ImageButton), default(int));
+
+		public static readonly BindableProperty ImageHeightRequestProperty =
+			BindableProperty.Create(
+					nameof(ImageHeightRequest), typeof(int), typeof(ImageButton), default(int));
+
+		public static readonly BindableProperty OrientationProperty =
+			BindableProperty.Create(
+					nameof(Orientation), typeof(ImageOrientation), typeof(ImageButton), ImageOrientation.ImageToLeft);
+
+		public static readonly BindableProperty ImageTintColorProperty =
+			BindableProperty.Create(
+					nameof(ImageTintColor), typeof(Color), typeof(ImageButton), Color.Transparent);
+
+		public static readonly BindableProperty DisabledImageTintColorProperty =
+			BindableProperty.Create(
+					nameof(DisabledImageTintColor), typeof(Color), typeof(ImageButton), Color.Transparent);
+
+		[TypeConverter(typeof(ImageSourceConverter))]
+		public ImageSource Source
+		{
+			get { return (ImageSource)GetValue(SourceProperty); }
+			set { SetValue(SourceProperty, value); }
+		}
+
+		[TypeConverter(typeof(ImageSourceConverter))]
+		public ImageSource DisabledSource
+		{
+			get { return (ImageSource)GetValue(DisabledSourceProperty); }
+			set { SetValue(DisabledSourceProperty, value); }
+		}
+
+		public ImageOrientation Orientation
+		{
+			get { return (ImageOrientation)GetValue(OrientationProperty); }
+			set { SetValue(OrientationProperty, value); }
+		}
+
+		public int ImageHeightRequest
+		{
+			get { return (int)GetValue(ImageHeightRequestProperty); }
+			set { SetValue(ImageHeightRequestProperty, value); }
+		}
+
+		public int ImageWidthRequest
+		{
+			get { return (int)GetValue(ImageWidthRequestProperty); }
+			set { SetValue(ImageWidthRequestProperty, value); }
+		}
+
+		public Color ImageTintColor
+		{
+			get { return (Color)GetValue(ImageTintColorProperty); }
+			set { SetValue(ImageTintColorProperty, value); }
+		}
+
+		public Color DisabledImageTintColor
+		{
+			get { return (Color)GetValue(DisabledImageTintColorProperty); }
+			set { SetValue(DisabledImageTintColorProperty, value); }
+		}
+	}
+}
---
+++ Radovan/Radovan.Plugin.Core/Enums/ImageOrientation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Radovan.Plugin.Core.Enums
+{
+	public enum ImageOrientation
+	{
+		ImageToLeft = 0,
+		ImageOnTop = 1,
+		ImageToRight = 2,
+		ImageOnBottom = 3,
+		ImageCentered = 4
+	}
+}
