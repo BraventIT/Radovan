94cbd7302971349cd7d9b6f8d1ee929d16922fb6
Mario López Baratas <mlopez@bravent.net>
2017-04-24 23:06:06 +0200

    Tarea 14501:Control Button

---
+++ Radovan/Radovan.Plugin.Android/Renderers/NoBorderButtonRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.Graphics;
+using Radovan.Plugin.Android.Renderers;
+using Radovan.Plugin.Core.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(NoBorderButton), typeof(NoBorderButtonRenderer))]
+namespace Radovan.Plugin.Android.Renderers
+{
+	public class NoBorderButtonRenderer : ButtonRenderer
+	{
+		protected override void OnDraw(Canvas canvas)
+		{
+			base.OnDraw(canvas);
+		}
+	}
+}
---
+++ Radovan/Radovan.Plugin.Core/Controls/NoBorderButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Radovan.Plugin.Core.Controls
+{
+	public class NoBorderButton : Button
+	{
+		#region SourcePlatform Bindable Property
+		public ImageSource SourcePlatform
+		{
+			get { return (ImageSource)GetValue(SourcePlatformProperty); }
+			set { SetValue(SourcePlatformProperty, value); }
+		}
+
+		public static readonly BindableProperty SourcePlatformProperty = BindableProperty.Create(
+			propertyName: nameof(SourcePlatform),
+			returnType: typeof(ImageSource),
+			declaringType: typeof(ExtendedImage),
+			defaultValue: default(ImageSource),
+			propertyChanged: OnSourcePlatformChanged);
+
+		private static void OnSourcePlatformChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var SourcePlatform = newValue as FileImageSource;
+			string path = string.Empty;
+			switch (Device.OS)
+			{
+				case TargetPlatform.WinPhone:
+				case TargetPlatform.Windows:
+					path = $"Assets/{SourcePlatform.File}.png"; break;
+				default:
+					path = SourcePlatform.File; break;
+			}
+			SourcePlatform.File = path;
+			bindable.SetValue(ImageProperty, SourcePlatform);
+		}
+		#endregion
+	}
+}
