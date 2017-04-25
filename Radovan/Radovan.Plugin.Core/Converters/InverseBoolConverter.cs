1a8248e94c9136e58381418c15b975217d63d82b
ALBERTO FRAJ SOUTO <afraj@bravent.net>
2017-04-25 11:23:50 +0200

    Inverse bool converter
    Related Work Items: #14506

---
+++ Radovan/Radovan.Plugin.Core/Converters/InverseBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Radovan.Plugin.Core.Converters
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return !(bool)value;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
