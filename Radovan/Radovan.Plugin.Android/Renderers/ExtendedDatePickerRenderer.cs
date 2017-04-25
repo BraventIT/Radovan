2616eadbe48c3fe9eca94eaa05603c2479b14ad4
ALBERTO FRAJ SOUTO <afraj@bravent.net>
2017-04-25 11:13:33 +0200

    Control DatePicker
    Related Work Items: #14497

---
+++ Radovan/Radovan.Plugin.Android/Renderers/ExtendedDatePickerRenderer.cs
@@ -0,0 +1,120 @@
+﻿using Android.Views;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Radovan.Plugin.Core.Controls;
+using Radovan.Plugin.Android.Renderers;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(ExtendedDatePicker), typeof(ExtendedDatePickerRenderer))]
+namespace Radovan.Plugin.Android.Renderers
+{
+    /// <summary>
+    ///  Extended DatePicker Renderer for Nullable Values
+    ///  Via: https://forums.xamarin.com/discussion/20028/datepicker-possible-to-bind-to-nullable-date-value
+    ///  Via: https://github.com/XLabs/Xamarin-Forms-Labs/wiki/ExtendedEntry
+    /// </summary>
+    public class ExtendedDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            var view = Element as ExtendedDatePicker;
+
+            if (view != null)
+            {
+                SetFont(view);
+                SetTextAlignment(view);
+                // SetBorder(view);
+                SetNullableText(view);
+                SetPlaceholder(view);
+                SetPlaceholderTextColor(view);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            var view = (ExtendedDatePicker)Element;
+
+            if (e.PropertyName == ExtendedDatePicker.FontProperty.PropertyName)
+                SetFont(view);
+            else if (e.PropertyName == ExtendedDatePicker.XAlignProperty.PropertyName)
+                SetTextAlignment(view);
+            // else if (e.PropertyName == ExtendedDatePicker.HasBorderProperty.PropertyName)
+            //  SetBorder(view);
+            else if (e.PropertyName == ExtendedDatePicker.NullableDateProperty.PropertyName)
+                SetNullableText(view);
+            else if (e.PropertyName == ExtendedDatePicker.PlaceholderProperty.PropertyName)
+                SetPlaceholder(view);
+            else if (e.PropertyName == ExtendedDatePicker.PlaceholderTextColorProperty.PropertyName)
+                SetPlaceholderTextColor(view);
+
+        }
+
+        /// <summary>
+        /// Sets the text alignment.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        private void SetTextAlignment(ExtendedDatePicker view)
+        {
+            switch (view.XAlign)
+            {
+                case Xamarin.Forms.TextAlignment.Center:
+                    Control.Gravity = GravityFlags.CenterHorizontal;
+                    break;
+                case Xamarin.Forms.TextAlignment.End:
+                    Control.Gravity = GravityFlags.End;
+                    break;
+                case Xamarin.Forms.TextAlignment.Start:
+                    Control.Gravity = GravityFlags.Start;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Sets the font.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        private void SetFont(ExtendedDatePicker view)
+        {
+            if (view.Font != Font.Default)
+            {
+                Control.TextSize = view.Font.ToScaledPixel();
+                //Control.Typeface = view.Font.ToExtendedTypeface(Context);
+            }
+        }
+
+        /// <summary>
+        /// Set text based on nullable value
+        /// </summary>
+        /// <param name="view"></param>
+        private void SetNullableText(ExtendedDatePicker view)
+        {
+            if (view.NullableDate == null)
+                Control.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Set the placeholder
+        /// </summary>
+        /// <param name="view"></param>
+        private void SetPlaceholder(ExtendedDatePicker view)
+        {
+            Control.Hint = view.Placeholder;
+        }
+
+        /// <summary>
+        /// Sets the color of the placeholder text.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        private void SetPlaceholderTextColor(ExtendedDatePicker view)
+        {
+            if (view.PlaceholderTextColor != Color.Default)
+            {
+                Control.SetHintTextColor(view.PlaceholderTextColor.ToAndroid());
+            }
+        }
+    }
+}
---
+++ Radovan/Radovan.Plugin.Core/Controls/ExtendedDatePicker.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Radovan.Plugin.Core.Controls
+{
+    /// <summary>
+	///  Extended DatePicker for Nullable Values
+	///  Via: https://forums.xamarin.com/discussion/20028/datepicker-possible-to-bind-to-nullable-date-value
+	///  Via: https://github.com/XLabs/Xamarin-Forms-Labs/wiki/ExtendedEntry
+	/// </summary>
+	public class ExtendedDatePicker : DatePicker
+    {
+        #region Font Bindable Property
+        public Font Font
+        {
+            get { return (Font)GetValue(FontProperty); }
+            set { SetValue(FontProperty, value); }
+        }
+
+        public static readonly BindableProperty FontProperty = BindableProperty.Create(
+            propertyName: nameof(Font),
+            returnType: typeof(Font),
+            declaringType: typeof(ExtendedDatePicker),
+            defaultValue: default(Font));
+        #endregion
+
+        #region NullableDate Bindable Property
+        public DateTime? NullableDate
+        {
+            get { return (DateTime?)GetValue(NullableDateProperty); }
+            set
+            {
+                SetValue(NullableDateProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// The NullableDate property
+        /// </summary>
+        public static readonly BindableProperty NullableDateProperty = BindableProperty.Create(
+            propertyName: nameof( NullableDate),
+            returnType: typeof(DateTime?),
+            declaringType: typeof(ExtendedDatePicker),
+            defaultValue: null, 
+            defaultBindingMode:BindingMode.TwoWay,
+            propertyChanged: OnNullableDateChanged);
+
+        private static void OnNullableDateChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var datePicker = bindable as ExtendedDatePicker;
+
+            datePicker?.UpdateDate();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The XAlign property
+        /// </summary>
+        public static readonly BindableProperty XAlignProperty =
+            BindableProperty.Create("XAlign", typeof(TextAlignment), typeof(ExtendedDatePicker),
+            TextAlignment.Start);
+
+        /// <summary>
+        /// The HasBorder property
+        /// </summary>
+        public static readonly BindableProperty HasBorderProperty =
+            BindableProperty.Create("HasBorder", typeof(bool), typeof(ExtendedDatePicker), true);
+
+        /// <summary>
+        /// The Placeholder property
+        /// </summary>
+        public static readonly BindableProperty PlaceholderProperty =
+            BindableProperty.Create("Placeholder", typeof(string), typeof(ExtendedDatePicker), string.Empty, BindingMode.OneWay);
+
+        /// <summary>
+        /// The PlaceholderTextColor property
+        /// </summary>
+        public static readonly BindableProperty PlaceholderTextColorProperty =
+            BindableProperty.Create("PlaceholderTextColor", typeof(Color), typeof(ExtendedDatePicker), Color.Default);
+
+        /// <summary>
+        /// Get or sets the NullableDate
+        /// </summary>
+      
+
+        /// <summary>
+        /// Gets or sets the X alignment of the text
+        /// </summary>
+        public TextAlignment XAlign
+        {
+            get { return (TextAlignment)GetValue(XAlignProperty); }
+            set { SetValue(XAlignProperty, value); }
+        }
+
+
+        /// <summary>
+        /// Gets or sets if the border should be shown or not
+        /// </summary>
+        public bool HasBorder
+        {
+            get { return (bool)GetValue(HasBorderProperty); }
+            set { SetValue(HasBorderProperty, value); }
+        }
+
+        /// <summary>
+        /// Get or sets the PlaceHolder
+        /// </summary>
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        /// <summary>
+        /// Sets color for placeholder text
+        /// </summary>
+        public Color PlaceholderTextColor
+        {
+            get { return (Color)GetValue(PlaceholderTextColorProperty); }
+            set { SetValue(PlaceholderTextColorProperty, value); }
+        }
+
+       
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            UpdateDate();
+        }
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            Device.OnPlatform(() =>
+            {
+                if (propertyName == IsFocusedProperty.PropertyName)
+                {
+                    if (IsFocused)
+                    {
+                        if (!NullableDate.HasValue)
+                        {
+                            Date = (DateTime)DateProperty.DefaultValue;
+                        }
+                    }
+                    else
+                    {
+                        OnPropertyChanged(DateProperty.PropertyName);
+                    }
+                }
+            });
+
+            if (propertyName == DateProperty.PropertyName)
+            {
+                NullableDate = Date;
+            }
+
+            if (propertyName == NullableDateProperty.PropertyName)
+            {
+                if (NullableDate.HasValue)
+                {
+                    Date = NullableDate.Value;
+                }
+            }
+        }
+
+        private void UpdateDate()
+        {
+            Date = (NullableDate.HasValue)?
+            NullableDate.Value : (DateTime)DateProperty.DefaultValue;
+        }
+    }
+}
