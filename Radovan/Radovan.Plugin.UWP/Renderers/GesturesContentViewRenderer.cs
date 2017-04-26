2c2a75ceedde5e10bdc86e537e32400c4cf24772
MARIO LOPEZ BARATAS <mlopez@bravent.net>
2017-04-26 17:16:37 +0200

    Gesture renderer for UWP

---
+++ Radovan/Radovan.Plugin.UWP/Renderers/GesturesContentViewRenderer.cs
@@ -0,0 +1,46 @@
+﻿using Radovan.Plugin.Core.Behaviors;
+using Radovan.Plugin.Core.Controls;
+using Radovan.Plugin.Core.Enums;
+using Radovan.Plugin.UWP.Renderers;
+using Windows.UI.Xaml;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(GesturesContentView), typeof(GesturesContentViewRenderer))]
+namespace Radovan.Plugin.UWP.Renderers
+{
+    public class GesturesContentViewRenderer : ViewRenderer<GesturesContentView, FrameworkElement>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<GesturesContentView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement == null)
+            {
+                Tapped -= GesturesContentViewRenderer_Tapped;
+                Holding -= GesturesContentViewRenderer_Holding;
+            }
+            if (e.OldElement != null) return;
+            Tapped += GesturesContentViewRenderer_Tapped;
+            Holding += GesturesContentViewRenderer_Holding;
+        }
+
+        private void GesturesContentViewRenderer_Holding(object sender, Windows.UI.Xaml.Input.HoldingRoutedEventArgs e)
+        {
+            Element.ProcessGesture(new GestureResult
+            {
+                ViewStack = null,
+                GestureType = GestureType.LongPress,
+                Direction = Directionality.None,
+            });
+        }
+
+        private void GesturesContentViewRenderer_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            Element.ProcessGesture(new GestureResult
+            {
+                ViewStack = null,
+                GestureType = GestureType.SingleTap,
+                Direction = Directionality.None,
+            });
+        }
+    }
+}
